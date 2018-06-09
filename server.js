e0f7339680511ef819821e6ac66b01783012ffae
Forty Morgan <dimazhmudikov@gmail.com>
2018-06-09 22:06:51 +0300

    Wrong syntax

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
-app.use(express.static('static'));
+app.use('/static/', express.static('static'));
+app.use('/foolowing/:user', express.static('static'));
+
+app.get('/', (req, res) => {
+  fs.readFile('./static/index.html', 'utf8', (err, data) => {
+    res.send(data);
+  })
+})
+
+app.get('/following/:user', (req, res) => {
+  res.send(req.params);
+})
 
 app.listen(3000);
