20b4c23c617bbf90c4e05ba6f59f66ff1b48842c
Forty Morgan <dimazhmudikov@gmail.com>
2018-06-10 17:45:24 +0300

    Another wrong try

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ const path = require('path');
 
 const app = express();
 
-app.use('/static', express.static('static'));
-app.get('/', (req, res) => {
+app.use('/static', (req, res) => {
+  res.sendFile(path.join(__dirname, "static" + req.path));
+});
+
+app.use('*', (req, res) => {
   res.sendFile(path.join(__dirname, "static/index.html"));
 });
+// app.use('/', express.static('static'));
+
 
 app.listen(3000);
